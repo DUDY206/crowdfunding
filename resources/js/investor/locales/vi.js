78520664165a3b23031a975d7aa80bee95560cff
bestb-tech <tech.bestb@gmail.com>
2021-04-16 17:39:25 +0700

    fix-invest-10-detail-invest

--- resources/js/investor/locales/vi.js
+++ resources/js/investor/locales/vi.js
@@ -157,4 +157,12 @@ export default {
     login: {
         'fail': 'Sai thông tin đăng nhập'
     },
+    about: {
+        'bestb': 'Hệ sinh thái Doanh nghiệp BestB',
+        'content_bestb': 'Với mục tiêu phát triển hệ sinh thái doanh nghiệp Việt đã luôn cố gắng, nỗ lực đồng hành cùng các doanh nghiệp Việt, các nhà đầu tư trong và ngoài nước. Chúng tôi luôn hướng tới cung cấp những giá trị tốt đẹp góp phần kiến tạo giá trị doanh nghiệp Việt',
+        'crowd': 'BestB Crowdfunding (Gọi vốn cộng đồng)',
+        'content_crowd': 'Là một kênh tài chính tiềm năng, là một trang gọi vốn cộng đồng nhằm kết nối các doanh nghiệp StartUp, Doanh nghiệp SMEs, doanh nghiệp cần gọi vốn và các nhà đầu tư với nhau. Các dự án gọi vốn cộng đồng được gửi tới BestB Crowdfunding gồm đa dạng các lĩnh vực khác nhau như: Thương mại điện tử, công nghệ thanh toán, thiết bị y tế, hàng tiêu dùng, nền tảng vận chuyển thông minh, Logistic, nông sản xuất khẩu, nền tảng giáo dục giới thiệu việc làm thông minh, cung ứng xe hơi, công nghệ sinh học, nhà thông minh....',
+        'vision': 'Tầm nhìn',
+        'content_vision': 'Với tầm nhìn sẽ trở thành một trong những trang mạng gọi vốn cộng đồng lớn nhất tại Việt Nam. Sẽ là một trong những trang Crowdfunding kết nối các doanh nghiệp với Nhà đầu tư đáng tin cậy và hiệu quả nhất. Giải quyết vấn đề tài chính cho doanh nghiệp, đem đến cho các Nhà đầu tư những doanh nghiệp tiềm năng từ đó doanh nghiệp có thể phát triển lớn mạnh, nhà đầu tư gia tăng vốn và đầu tư cho các doanh nghiệp khác giúp cho các thương hiệu Việt kiến thiết tương lai - Vươn tầm Quốc tế.',
+    },
 }
--- resources/js/investor/pages/AboutBestB.vue
+++ resources/js/investor/pages/AboutBestB.vue
@@ -4,23 +4,23 @@
         <div v-if="!isLoading" class="title-page">{{ $t('header_banner.about') }}</div>
         <div v-if="!isLoading" class="wrapper-boxs">
             <div class="box">
-                <div class="title">Hệ sinh thái Doanh nghiệp BestB</div>
+                <div class="title">{{ $t('about.bestb') }}</div>
                 <div class="content">
-                    Với mục tiêu phát triển hệ sinh thái doanh nghiệp Việt đã luôn cố gắng, nỗ lực đồng hành cùng các doanh nghiệp Việt, các nhà đầu tư trong và ngoài nước. Chúng tôi luôn hướng tới cung cấp những giá trị tốt đẹp góp phần kiến tạo giá trị doanh nghiệp Việt
+                    {{ $t('about.bestb') }}
                 </div>
             </div>
             <hr />
             <div class="box">
-                <div class="title">BestB Crowdfunding</div>
+                <div class="title">{{ $t('about.crowd') }}</div>
                 <div class="content">
-                    Là một kênh tài chính tiềm năng, là một trang gọi vốn cộng đồng nhằm kết nối các doanh nghiệp StartUp, Doanh nghiệp SMEs, doanh nghiệp cần gọi vốn và các nhà đầu tư với nhau. Các dự án gọi vốn cộng đồng được gửi tới BestB Crowdfunding gồm đa dạng các lĩnh vực khác nhau như: Thương mại điện tử, công nghệ thanh toán, thiết bị y tế, hàng tiêu dùng, nền tảng vận chuyển thông minh, Logistic, nông sản xuất khẩu, nền tảng giáo dục giới thiệu việc làm thông minh, cung ứng xe hơi, công nghệ sinh học, nhà thông minh....
+                    {{ $t('about.content_crowd') }}
                 </div>
             </div>
             <hr />
             <div class="box">
-                <div class="title">Tầm nhìn</div>
+                <div class="title">{{ $t('about.vision') }}</div>
                 <div class="content">
-                    Với tầm nhìn sẽ trở thành một trong những trang mạng gọi vốn cộng đồng lớn nhất tại Việt Nam. Sẽ là một trong những trang Crowdfunding kết nối các doanh nghiệp với Nhà đầu tư đáng tin cậy và hiệu quả nhất. Giải quyết vấn đề tài chính cho doanh nghiệp, đem đến cho các Nhà đầu tư những doanh nghiệp tiềm năng từ đó doanh nghiệp có thể phát triển lớn mạnh, nhà đầu tư gia tăng vốn và đầu tư cho các doanh nghiệp khác giúp cho các thương hiệu Việt kiến thiết tương lai - Vươn tầm Quốc tế.
+                    {{ $t('about.content_vision') }}
                 </div>
             </div>
             <hr />
