38605368f7e3ce8a5bb1ccdf117b177831ebc602
bestb-tech <tech.bestb@gmail.com>
2021-04-22 11:53:56 +0700

    update-invest

--- resources/views/investor/app.blade.php
+++ resources/views/investor/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/png" sizes="96x96" href="/favicon.png">
-    <link rel="stylesheet" href="/investor/css/style.css" />
+    <link rel="stylesheet" href="{{ asset('investor/css/style.css?v='.time()) }}" />
     <script src="{{ asset('/bower_components/jquery/dist/jquery.js') }}" ></script>
     <link rel="stylesheet" href="{{ asset('/bower_components/Font-Awesome/css/all.min.css?v='.time()) }}">
 
