c34fc3e689415945fd918a42b8431d0eeaad62a8
bestb-tech <tech.bestb@gmail.com>
2021-07-05 11:49:25 +0700

    update-code

--- resources/views/investor/pdf/contract.blade.php
+++ resources/views/investor/pdf/contract.blade.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <style>
-        @page {
+        /* @page {
             margin-top: 3cm;
             margin-bottom: 4cm;
         }
@@ -38,6 +38,31 @@
 
         .flyleaf {
             page-break-after: always;
+        } */
+
+
+        @page {
+            margin-top: 3cm;
+            margin-left: 2cm;
+            margin-right: 2cm;
+        }
+
+        body {
+            font-family: 'myFont';
+            font-size: 14px;
+        }
+
+        header {
+            position: fixed;
+            top: -3cm;
+            left: -1.2cm;
+            right: -1.2cm;
+            height: 115px;
+            width: 100%;
+            background: url('{{ public_path('images/pdf/header1.png') }}');
+            background-size: cover;
+            background-repeat: no-repeat;
+            margin: 0cm 0cm;
         }
     </style>
 </head>
@@ -50,6 +75,6 @@
         {!! $contract !!}
     </div>
 
-    <footer></footer>
+    {{-- <footer></footer> --}}
 </body>
 </html>
